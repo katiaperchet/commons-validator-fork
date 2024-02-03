9703d532e4a19587277c6a60667d4061d4da6c12
Katia Perchet <53318951+katiaperchet@users.noreply.github.com>
2024-02-03 23:25:30 +0100

    Changed visibility of AbstractFormatValidator constructor

--- src/main/java/org/apache/commons/validator/routines/AbstractFormatValidator.java
+++ src/main/java/org/apache/commons/validator/routines/AbstractFormatValidator.java
@@ -44,7 +44,7 @@ public abstract class AbstractFormatValidator implements Serializable {
      * @param strict {@code true} if strict
      *        <code>Format</code> parsing should be used.
      */
-    public AbstractFormatValidator(final boolean strict) {
+    protected AbstractFormatValidator(final boolean strict) {
         this.strict = strict;
     }
 
